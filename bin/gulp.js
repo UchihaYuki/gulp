49b8f7f43603e55a94ed01b373d8b0e9a6e7c682
yuki <200706991@qq.com>
2024-06-21 23:42:23 +0800

    transpile gulpfile.ts first if it exists

--- bin/gulp.js
+++ bin/gulp.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-require('gulp-cli')();
+const fs = require('fs');
+if (fs.existsSync('./gulpfile.ts')) {
+  const { spawn } = require('child_process');
+  const child = spawn(
+    `npx tsc -p ./tsconfig.gulp.json && npx tsc-alias -p ./tsconfig.gulp.json`,
+    { shell: true, stdio: 'inherit' }
+  );
+  child.on('exit', (code, signal) => {
+    if (!code && !signal) {
+      require('gulp-cli')();
+    }
+  });
+} else {
+  require('gulp-cli')();
+}
